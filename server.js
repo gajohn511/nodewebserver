3ad0ad49469b505005d6e872914efdcd2a439f0d
John Garzon <gajohn511@gmail.com>
2017-07-17 23:21:27 -0400

    Setup heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hbs = require("hbs");
 const fs = require("fs");
 
+const port = process.env.PORT || 3000;
 var app = express();
 hbs.registerPartials(__dirname + "/views/partials");
 app.set("view engine", "hbs");
@@ -52,6 +53,6 @@ app.get("/about", (req, res) => {
   });
 });
 
-app.listen(3000, undefined, undefined, () => {
-  console.log("Listening on port 3000");
+app.listen(port, undefined, undefined, () => {
+  console.log(`Listening on port ${port}`);
 });
